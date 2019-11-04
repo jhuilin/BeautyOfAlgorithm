7e3b46f31937efdb5c81667000ee59b2ecba7b74
jianhui <jianhuilin1124@gmail.com>
2019-11-04 13:40:48 -0500

    completed buble sort

--- beauty_of_algorithms/src/Visualizer/Visualizer.jsx
+++ beauty_of_algorithms/src/Visualizer/Visualizer.jsx
@@ -2,22 +2,24 @@ import React from "react";
 import "./Visualizer.css";
 import { getBubbleSortAnimations } from "../Algorithms/BubbleSort";
 
-let window_width = window.innerWidth;
-let window_height = window.innerHeight;
-let array_size = parseInt((window_width - 200) / 8);
+let WINDOW_WIDTH = window.innerWidth;
+let WINDOW_HEIGHT = window.innerHeight;
+let NUMBER_OF_ARRAY_BARS = parseInt((WINDOW_WIDTH - 200) / 8);
 
 function reportWindowSize() {
-  window_width = window.innerWidth;
-  window_height = window_height;
-  array_size = parseInt((window_width - 200) / 8);
+  WINDOW_WIDTH = window.innerWidth;
+  WINDOW_HEIGHT = window.innerHeight;
+  NUMBER_OF_ARRAY_BARS = parseInt((WINDOW_WIDTH - 200) / 8);
 }
 window.onresize = reportWindowSize;
 
-const primary_color = "white";
-const secondary_color = "red";
-const animation_speed = 10;
-const disabled_buttons = "currently disabled";
-const enable_buttons = {
+const PRIMARY_COLOR = "white";
+const SECONDARY_COLOR = "red";
+const ANIMATION_SPEED_MS = 0.001;
+
+//Tooltips for buttons
+const DISABLED_BUTTON = "Currently Disabled";
+const ENABLED_BUTTON = {
   nlogn: "O(NlogN) Time Complexity",
   nSquare: "O(N^2) Time Complexity"
 };
@@ -29,53 +31,47 @@ class Visualizer extends React.Component {
       array: []
     };
   }
-
   componentDidMount() {
-    this.reserArray();
+    this.resetArray();
   }
-  randomIntFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
-
-  reserArray = () => {
+  //Generates new random array
+  resetArray() {
     const array = [];
-    for (let i = 0; i < array_size; i++) {
-      array.push(this.randomIntFromInterval(25, window_height - 30));
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomIntFromInterval(25, WINDOW_HEIGHT - 30));
     }
     this.setState({ array: array });
-    this.restoreButtons();
-  };
-
-  disableButtons = () => {
+    this.restoreStoreButtons();
+  }
+  disableSortButtons() {
     document.getElementById("bubbleSort").disabled = true;
     // buttonStyle = document.getElementById("bubbleSort").style;
-    // document.getElementById("bubbleSort").title = disabled_buttons;
+    // document.getElementById("bubbleSort").title = DISABLED_BUTTON;
     // buttonStyle.cursor = "default";
     // buttonStyle.background = "#000000";
-  };
-  restoreButtons = () => {
+  }
+  restoreStoreButtons() {
     document.getElementById("bubbleSort").disabled = false;
     // buttonStyle = document.getElementById("bubbleSort").style;
-    // document.getElementById("bubbleSort").title = enable_buttons.nSquare;
+    // document.getElementById("bubbleSort").title = ENABLED_BUTTON.nSquare;
     // buttonStyle.background = "#47535E";
     // buttonStyle.cursor = "pointer";
-  };
-
-  bubbleSort = () => {
+  }
+  bubbleSort() {
     this.disableSortButtons();
     const [animations, sortArray] = getBubbleSortAnimations(this.state.array);
     for (let i = 0; i < animations.length; i++) {
       const isColorChange = i % 4 === 0 || i % 4 === 1;
       const arrayBars = document.getElementsByClassName("array-bar");
       if (isColorChange === true) {
-        const color = i % 4 === 0 ? secondary_color : primary_color;
+        const color = i % 4 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
         const [barOneIndex, barTwoIndex] = animations[i];
         const barOneStyle = arrayBars[barOneIndex].style;
         const barTwoStyle = arrayBars[barTwoIndex].style;
         setTimeout(() => {
           barOneStyle.backgroundColor = color;
           barTwoStyle.backgroundColor = color;
-        }, i * animation_speed);
+        }, i * ANIMATION_SPEED_MS);
       } else {
         const [barIndex, newHeight] = animations[i];
         if (barIndex === -1) {
@@ -84,17 +80,19 @@ class Visualizer extends React.Component {
         const barStyle = arrayBars[barIndex].style;
         setTimeout(() => {
           barStyle.height = `${newHeight}px`;
-        }, i * animation_speed);
+        }, i * ANIMATION_SPEED_MS);
       }
     }
     // this.setState({array: sortArray})
     const RESTORE_TIME = parseInt(
-      (animation_speed * animations.length) / 2 + 3000
+      (ANIMATION_SPEED_MS * animations.length) / 2 + 3000
     );
-    setTimeout(() => this.restoreButtons(), RESTORE_TIME);
-  };
+    setTimeout(() => this.restoreStoreButtons(), RESTORE_TIME);
+  }
   render() {
     const array = this.state.array;
+    const SORT_BUTTONS = 6;
+    const TOTAL_BUTTONS = 1 + SORT_BUTTONS;
     return (
       <>
         <div
@@ -106,31 +104,30 @@ class Visualizer extends React.Component {
               className="array-bar"
               key={idx}
               style={{
-                backgroundColor: primary_color,
+                backgroundColor: PRIMARY_COLOR,
                 height: `${value}px`
               }}
             ></div>
           ))}
         </div>
+
         <div className="buttons">
           <button
-            title="Array Shuffling"
+            title="Generates a new random array"
             style={{
               position: "relative",
-              top: `${0 * (window_height - 20)}px`
+              top: `${(0 * (WINDOW_HEIGHT - 20)) / TOTAL_BUTTONS}px`
             }}
             onClick={() => this.resetArray()}
           >
             Generate New Array
           </button>
-        </div>
-        <div className="buttons">
           <button
             title="O(N^2) Time Complexity"
             id="bubbleSort"
             style={{
               position: "relative",
-              top: `${2.5 * (window_height - 20)}px`
+              top: `${(2.5 * (WINDOW_HEIGHT - 20)) / TOTAL_BUTTONS}px`
             }}
             onClick={() => this.bubbleSort()}
           >
@@ -140,6 +137,12 @@ class Visualizer extends React.Component {
       </>
     );
   }
+}
+
+// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
+function randomIntFromInterval(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export default Visualizer;
