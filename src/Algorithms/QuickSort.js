de6e0477770be37d06c2016a6f79630031a553ff
jianhui <jianhuilin1124@gmail.com>
2019-11-05 22:28:43 -0500

    new one

---
+++ src/Algorithms/QuickSort.js
@@ -0,0 +1,49 @@
+import { isEqual } from "./helper/IsEqual";
+import { swap } from "./helper/swap";
+
+export function getQuickSortAnimations(array) {
+  let animations = [];
+  let auxillaryArray = array.slice();
+  quickSort(auxillaryArray, 0, auxillaryArray.length - 1, animations);
+  const sortedArray = array.slice().sort((a, b) => a - b);
+  console.log("sort works correctly? ", isEqual(sortedArray, auxillaryArray));
+  return animations;
+}
+
+function quickSort(auxillaryArray, startIndex, endIndex, animations) {
+  let pivotIndex;
+  if (startIndex < endIndex) {
+    pivotIndex = partitionArray(
+      auxillaryArray,
+      startIndex,
+      endIndex,
+      animations
+    );
+    quickSort(auxillaryArray, startIndex, pivotIndex - 1, animations);
+    quickSort(auxillaryArray, pivotIndex + 1, endIndex, animations);
+  }
+}
+
+function partitionArray(auxillaryArray, startIndex, endIndex, animations) {
+  let pivot = auxillaryArray[endIndex];
+  let pivotIndex = startIndex;
+  for (let i = startIndex; i <= endIndex - 1; i++) {
+    animations.push(["comparision1", i, endIndex]);
+    animations.push(["comparision1", i, pivotIndex]);
+    animations.push(["comparision2", i, endIndex]);
+    animations.push(["comparision2", i, pivotIndex]);
+
+    if (auxillaryArray[i] <= pivot) {
+      //Swap these two heights
+      animations.push(["swap", i, auxillaryArray[pivotIndex]]);
+      animations.push(["swap", pivotIndex, auxillaryArray[i]]);
+      swap(auxillaryArray, i, pivotIndex);
+      pivotIndex++;
+    }
+  }
+  //Swap these two heights
+  animations.push(["swap", pivotIndex, auxillaryArray[endIndex]]);
+  animations.push(["swap", endIndex, auxillaryArray[pivotIndex]]);
+  swap(auxillaryArray, pivotIndex, endIndex);
+  return pivotIndex;
+}
