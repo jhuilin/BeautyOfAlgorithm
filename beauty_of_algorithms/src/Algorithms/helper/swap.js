5143371d99f172c41fb5382cc159e82138a854ae
jianhui <jianhuilin1124@gmail.com>
2019-11-05 22:25:27 -0500

    fixed

--- beauty_of_algorithms/src/Algorithms/helper/swap.js
+++
@@ -1,5 +0,0 @@
-export function swap(auxillaryArray, firstIndex, secondIndex) {
-  let temp = auxillaryArray[firstIndex];
-  auxillaryArray[firstIndex] = auxillaryArray[secondIndex];
-  auxillaryArray[secondIndex] = temp;
-}
